ea1a630a01437a2ff4a111e9760036194260a3a2
beemhuse <brightawah94@gmail.com>
2026-03-05 04:19:17 +0000

    new migrations

---
+++ prisma/migrations/20260305041845_init/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[verification_token]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "is_admin" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "is_verified" BOOLEAN DEFAULT false,
+ADD COLUMN     "verification_token" TEXT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_verification_token_key" ON "users"("verification_token");
